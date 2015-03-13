9423ae6354599f5d78ef471e99552be03d74e337
Slash705 <alexandre.ronse@gmail.com>
2015-03-13 18:17:47 +0100

    Added code

---
+++ CheatyJSON/CheatyJSON.h
@@ -0,0 +1,19 @@
+//
+//  CheatyJSON.h
+//  CheatyJSON
+//
+//  Created by Alexandre Ronse on 13/03/2015.
+//  Copyright (c) 2015 Alexandre Ronse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CheatyJSON.
+FOUNDATION_EXPORT double CheatyJSONVersionNumber;
+
+//! Project version string for CheatyJSON.
+FOUNDATION_EXPORT const unsigned char CheatyJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheatyJSON/PublicHeader.h>
+
+
